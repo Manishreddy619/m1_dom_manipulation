9832ad0b178769af0130c7061f8ad08a4c399587
manishreddy619 <manishreddy916@gmail.com>
2021-06-29 16:41:14 +0200

    final changes

---
+++ script.js
@@ -0,0 +1,12 @@
+const changeH1 = () => {
+	let h1 = document.querySelector('h1');
+	h1.style.color = 'pink';
+};
+const backgroundColor = () => {
+	let body = document.querySelector('body');
+	body.style.backgroundColor = 'blue';
+};
+const addClass = () => {
+	let link = document.querySelectorAll('.phn_img');
+	link.className.add('amazonLink');
+};
